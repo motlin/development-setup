828623379a635584ea6fb9cb757f3bcf8233c574
Craig P. Motlin <cmotlin@gmail.com>
2026-06-23 17:11:04 -0400

    Add --rebase-merges to ri and replace graph aliases with l/laa/la/decorate and brighter rl format.

--- git-config-aliases.sh
+++ git-config-aliases.sh
@@ -5,7 +5,7 @@
 set -euxo pipefail
 
 # My aliases that aren't too common
-git config --global alias.ri 'rebase --interactive --autosquash'
+git config --global alias.ri 'rebase --interactive --autosquash --rebase-merges'
 git config --global alias.f 'fetch --all --prune --jobs=8'
 git config --global alias.detach 'checkout --detach'
 git config --global alias.ss 'status --short'
@@ -13,14 +13,23 @@ git config --global alias.m 'mergetool --no-prompt --tool=vimdiff'
 git config --global alias.exec '!exec '
 
 # Git log --graph aliases
-git config --global alias.gl "log --graph --decorate --date=short --format=format:'%C(yellow)%h%C(reset) %C(cyan)%ad%C(reset)%C(auto)%d%C(reset) %C(normal)%s%C(reset) %C(magenta)%an%C(reset) %C(green)%ar%C(reset)'"
-git config --global alias.ga "log --graph --decorate --date=short --format=format:'%C(yellow)%h%C(reset) %C(cyan)%ad%C(reset)%C(auto)%d%C(reset) %C(normal)%s%C(reset) %C(magenta)%an%C(reset) %C(green)%ar%C(reset)' --exclude=refs/remotes/upstream/pr/*/merge --all"
-git config --global alias.gb "log --graph --decorate --date=short --format=format:'%C(yellow)%h%C(reset) %C(cyan)%ad%C(reset)%C(auto)%d%C(reset) %C(normal)%s%C(reset) %C(magenta)%an%C(reset) %C(green)%ar%C(reset)' --branches"
+# Several variants of `git log --graph --oneline` with richer formatting:
+#   l        for specified branches or HEAD, e.g. `git l main`
+#   laa      for all refs
+#   la       like laa but filters notes, stash, dependabot, and pull-request refs
+#   decorate like la, but --simplify-by-decoration
+# Format: decorate prefix on its own line, dim abbreviated hash, notes, subject
+# truncated 50 from the right edge, author name (15), author date, relative date.
+git config --global alias.l "log --graph --decorate --date=short --format=format:'%C(auto)%(decorate:prefix=👇 ,suffix=%n,tag=,separator= )%C(reset)%C(dim)%h%C(reset) %N%-C()%C(normal)%<|(-50,trunc)%s%C(reset) %C(brightmagenta)%<(15,trunc)%an%C(reset) %C(brightblue)%ad%C(reset) %C(brightcyan)%ar%C(reset)'"
+git config --global alias.laa "l --all"
+# The exclude list is personal; tune the pr/dependabot/notes/retype refs to taste.
+git config --global alias.la "l --exclude=refs/prefetch/* --exclude=refs/remotes/origin/pr/* --exclude=refs/remotes/upstream/pr/* --exclude=refs/remotes/origin/dependabot/* --exclude=refs/notes/tests/* --exclude=refs/notes/commits --exclude=refs/stash --exclude=refs/remotes/origin/retype --all"
+git config --global alias.decorate "la --simplify-by-decoration"
 
 # reflog
 # Some people alias 'rl' to just 'reflog' without all these options
 # https://stackoverflow.com/a/17369548/23572
-git config --global alias.rl "reflog @{now} --date=relative --format=format:'%C(yellow)%h%C(reset) %C(cyan)%gd%C(reset) %C(green)%ar%C(reset)%C(auto)%d%C(reset) %C(magenta)%gs%C(reset) %C(normal)%s%C(reset)'"
+git config --global alias.rl "reflog @{now} --date=relative --format=format:'%C(brightyellow)%h%C(reset) %C(brightblue)%gd%C(reset) %C(brightcyan)%cr%C(reset)%C(auto)%d%C(reset) %C(brightmagenta)%gs%C(reset) %C(normal)%s%C(reset)'"
 
 # https://gist.github.com/DuaelFr/5663854
 git config --global alias.who 'shortlog --summary --numbered --email --no-merges'
